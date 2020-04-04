edf36b0c24e8d73f75a83521e4045983b4bf759c
jwalrus <jwalrus>
2020-04-04 19:49:44 -0400

    add simulation implementation - lots of clean up needed

--- montyhall.hs
+++ montyhall.hs
@@ -1,42 +1,78 @@
+import System.Environment
 import System.Random
 
-main = do
+main = do 
+    argList <- getArgs
+    dispatch argList
+
+dispatch :: [String] -> IO ()
+dispatch ("play":_) = playGame
+dispatch ("simulate":n:"keep":_) = runSimulation (read n) keepDoor
+dispatch ("simulate":n:"switch":_) = runSimulation (read n) switchDoor
+dispatch _ = help
+
+playGame = do
     gen <- getStdGen
     let (door, gen') = setupGame gen
         (choice, gen'') = contestantPickDoor gen'
-        remainingDoor = eliminateDoor gen'' door choice
+        (remainingDoor, gen''') = eliminateDoor gen'' door choice
     putStrLn $ "The car is behind door number " ++ show door
     putStrLn $ "The contestent chose door number " ++ show choice
     putStrLn $ "The remaining choice is " ++ show remainingDoor
-    putStrLn $ "You win a " ++ (keepDoor door choice)
+    let prize = keepDoor door choice
+    putStrLn $ "You win a " ++ prize
+    let (sim, _) = simulateGame gen switchDoor
+    putStrLn $ "simulation result = " ++ sim
+
+runSimulation :: Int -> (Int -> Int -> String) -> IO ()
+runSimulation n strategy = do
+    gen <- getStdGen
+    let wins = length $ filter (\x -> x == "car!!!") $ take n $ simulate gen strategy
+    putStrLn $ "won " ++ show wins ++ " out of " ++ show n ++ " games."
+
+help = do
+    putStrLn "TODO - create help message."
+
+simulate :: StdGen -> (Int -> Int -> String) -> [String]
+simulate gen strategy = 
+    let (prize, gen') = simulateGame gen strategy
+    in prize : simulate gen' strategy
+
+simulateGame :: StdGen -> (Int -> Int -> String) -> (String, StdGen)
+simulateGame gen strategy = 
+    let (door, gen') = setupGame gen
+        (choice, gen'') = contestantPickDoor gen'
+        (remainingDoor, gen''') = eliminateDoor gen'' door choice
+        prize = strategy door choice
+    in (prize, gen''')
 
 
-setupGame :: StdGen -> (Int, StdGen)
+setupGame :: StdGen ->  (Int, StdGen)
 setupGame gen = randomR (1,3) gen :: (Int, StdGen)
 
 contestantPickDoor :: StdGen -> (Int, StdGen)
 contestantPickDoor gen = randomR (1,3) gen :: (Int, StdGen)
 
-eliminateDoor :: StdGen -> Int -> Int -> Int
+eliminateDoor :: StdGen -> Int -> Int -> (Int, StdGen)
 eliminateDoor gen 1 1 = chooseOne gen [2,3]
 eliminateDoor gen 2 2 = chooseOne gen [1,3]
 eliminateDoor gen 3 3 = chooseOne gen [1,2]
-eliminateDoor _ 1 2 = 1
-eliminateDoor _ 1 3 = 1
-eliminateDoor _ 2 1 = 2
-eliminateDoor _ 2 3 = 2
-eliminateDoor _ 3 1 = 3
-eliminateDoor _ 3 2 = 3
+eliminateDoor gen 1 2 = (1, gen)
+eliminateDoor gen 1 3 = (1, gen)
+eliminateDoor gen 2 1 = (2, gen)
+eliminateDoor gen 2 3 = (2, gen)
+eliminateDoor gen 3 1 = (3, gen)
+eliminateDoor gen 3 2 = (3, gen)
 
-chooseOne :: StdGen -> [Int] -> Int
+chooseOne :: StdGen -> [Int] -> (Int, StdGen)
 chooseOne gen xs =
     let n = (length xs) - 1
-        (i, _) = randomR (0,n) gen :: (Int, StdGen)
-    in xs !! i
+        (i, gen') = randomR (0,n) gen :: (Int, StdGen)
+    in (xs !! i, gen')
 
 switchDoor :: Int -> Int -> String
 switchDoor car door 
-    | car == door = "car!!!"
+    | car /= door = "car!!!"
     | otherwise   = "goat :("
 
 keepDoor :: Int -> Int -> String
